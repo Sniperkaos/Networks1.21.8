633aec921f6efa29ad70a642aeae6d488192ff0c
cworldstar <legendaryhacker01@askusabouttaxes.com>
2025-12-23 14:12:52 -0500

    push to master

--- src/main/java/dev/cworldstar/networks/NetworksExtension.java
+++ src/main/java/dev/cworldstar/networks/NetworksExtension.java
@@ -6,16 +6,18 @@ import org.bukkit.inventory.ItemStack;
 import dev.cworldstar.networks.network.AdvancedNetworkPusher;
 import io.github.sefiraat.networks.utils.Theme;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.sefiraat.networks.Networks;
 import io.github.sefiraat.networks.slimefun.NetworksItemGroups;
+import io.github.sefiraat.networks.slimefun.NetworksSlimefunItemStacks;
 
 public class NetworksExtension {
 	public static void setup(Networks networks) {
 		new AdvancedNetworkPusher(
-				NetworksItemGroups.MAIN,
+				NetworksItemGroups.NETWORK_ITEMS,
 				Theme.themedSlimefunItemStack(
 					"NTW_ADVANCED_PUSHER", 
-					new ItemStack(Material.MAGENTA_STAINED_GLASS), 
+					new ItemStack(Material.BROWN_STAINED_GLASS), 
 					Theme.MACHINE, 
 			        "Advanced Network Pusher",
 			        "The ANP (Advanced Network Pusher)",
@@ -24,7 +26,9 @@ public class NetworksExtension {
 				),
 				RecipeType.ENHANCED_CRAFTING_TABLE,
 				new ItemStack[] {
-						
+					NetworksSlimefunItemStacks.ADVANCED_NANOBOTS.item(), NetworksSlimefunItemStacks.OPTIC_CABLE.item(), NetworksSlimefunItemStacks.OPTIC_CABLE.item(),
+					SlimefunItems.CARGO_OUTPUT_NODE_2.item(), NetworksSlimefunItemStacks.NETWORK_PUSHER.item(), NetworksSlimefunItemStacks.ADVANCED_NANOBOTS.item(),
+					NetworksSlimefunItemStacks.PRISTINE_AI_CORE.item(), NetworksSlimefunItemStacks.OPTIC_CABLE.item(), NetworksSlimefunItemStacks.OPTIC_GLASS.item()
 				}
 		).register(networks);
 	}
